6428a60325ef1edf5c4f8c0b99e9441276f39b5e
Mark Henderson <henderson.mark@gmail.com>
2019-09-13 16:02:49 -0400

    Resolving on closed cache
    Now opening caches, but not closing, on retrieval as a workaround
    
    No more opening after all

--- src/Cache.js
+++ src/Cache.js
@@ -7,9 +7,26 @@ Logger.setLogLevel('ERROR')
 class Cache {
   constructor (store) {
     this._store = store
+    this.handlers = new Set()
   }
 
-  async close () { await this._store.close() }
+  get status () { return this._store.db.status }
+
+  async close () {
+    if (!this._store) return Promise.reject(new Error('No cache store found to close'))
+    if (this.status === 'open') {
+      await this._store.close()
+      return Promise.resolve()
+    }
+  }
+
+  async open () {
+    if (!this._store) return Promise.reject(new Error('No cache store found to open'))
+    if (this.status !== 'open') {
+      await this._store.open()
+      return Promise.resolve()
+    }
+  }
 
   async get (key) {
     return new Promise((resolve, reject) => {
@@ -27,7 +44,7 @@ class Cache {
   }
 
   // Set value in the cache and return the new value
-  set (key, value) {
+  async set (key, value) {
     return new Promise((resolve, reject) => {
       this._store.put(key, JSON.stringify(value), (err) => {
         if (err) {
@@ -48,7 +65,7 @@ class Cache {
 
   // Remove a value and key from the cache
   async del (key) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       this._store.del(key, (err) => {
         if (err) {
           // Ignore error if key was not found
