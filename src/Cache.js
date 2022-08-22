67bfbbd773cafc2ceda70e31ce160e606b296e53
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-08-22 14:16:23 +0200

    refactor Cache.js to be more readable by removing Promise and using async functions instead

--- src/Cache.js
+++ src/Cache.js
@@ -12,51 +12,34 @@ class Cache {
   get status () { return this._store.db.status }
 
   async close () {
-    if (!this._store) return Promise.reject(new Error('No cache store found to close'))
+    if (!this._store) throw new Error('No cache store found to close')
+
     if (this.status === 'open') {
       await this._store.close()
-      return Promise.resolve()
     }
   }
 
   async open () {
-    if (!this._store) return Promise.reject(new Error('No cache store found to open'))
+    if (!this._store) throw new Error('No cache store found to open')
+
     if (this.status !== 'open') {
       await this._store.open()
-      return Promise.resolve()
     }
   }
 
   async get (key) {
-    return new Promise((resolve, reject) => {
-      this._store.get(key, (err, value) => {
-        if (err) {
-          // Ignore error if key was not found
-          if (err.toString().indexOf('NotFoundError: Key not found in database') === -1 &&
-            err.toString().indexOf('NotFound') === -1) {
-            return reject(err)
-          }
-        }
-        resolve(value ? JSON.parse(value) : null)
-      })
-    })
+    try {
+      return JSON.parse(await this._store.get(key))
+    } catch(e) {
+      if(e.code == 'LEVEL_NOT_FOUND') return null
+      throw e
+    }
   }
 
   // Set value in the cache and return the new value
-  set (key, value) {
-    return new Promise((resolve, reject) => {
-      this._store.put(key, JSON.stringify(value), (err) => {
-        if (err) {
-          // Ignore error if key was not found
-          if (err.toString().indexOf('NotFoundError: Key not found in database') === -1 &&
-            err.toString().indexOf('NotFound') === -1) {
-            return reject(err)
-          }
-        }
-        logger.debug(`cache: Set ${key} to ${JSON.stringify(value)}`)
-        resolve()
-      })
-    })
+  async set (key, value) {
+    await this._store.put(key, JSON.stringify(value))
+    logger.debug(`cache: Set ${key} to ${JSON.stringify(value)}`)
   }
 
   load () {} // noop
@@ -64,18 +47,7 @@ class Cache {
 
   // Remove a value and key from the cache
   async del (key) {
-    return new Promise((resolve, reject) => {
-      this._store.del(key, (err) => {
-        if (err) {
-          // Ignore error if key was not found
-          if (err.toString().indexOf('NotFoundError: Key not found in database') === -1 &&
-            err.toString().indexOf('NotFound') === -1) {
-            return reject(err)
-          }
-        }
-        resolve()
-      })
-    })
+    await this._store.del(key)
   }
 }
 
